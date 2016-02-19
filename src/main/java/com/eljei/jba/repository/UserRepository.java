3d5108f7e7f5d7ee0bd2d2a51fb30d502b99dd9b
Calma <Calma@Calma-PC.local>
2016-02-19 05:39:07 -0800

    commit

---
+++ src/main/java/com/eljei/jba/repository/UserRepository.java
@@ -0,0 +1,9 @@
+package com.eljei.jba.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.eljei.jba.entity.User;
+
+public interface UserRepository extends JpaRepository<User, Integer> {
+
+}
